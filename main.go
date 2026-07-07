106e2369b7b3af91b2d746057e9a67ee2a01e08a
Stanley-0ps <ochaistanley7@gmail.com>
2026-07-07 11:05:43 +0100

    ascii art web rebuild

--- main.go
+++ main.go
@@ -1,104 +1,22 @@
 package main
 
 import (
-	"ascii-art/functions"
-	"fmt"
-	"html/template"
-	"net/http"
-	"strings"
+	"ascii-art/server"
+	"embed"
 )
 
-type PageData struct {
-	Result string
-	Text   string
-	Banner string
-}
+//go:embed templates
+var templateFs embed.FS
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "404 page not found", http.StatusNotFound)
-		return
-	}
+//go:embed static
+var cssFiles embed.FS
 
-	tmpl, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		http.Error(w, "Template not found", http.StatusNotFound)
-		return
-	}
+// go build -ldflags "-X 'main.Style=K'" . (choose between A and k style)
+var Style = "A"
 
-	data := PageData{
-		Banner: "standard",
-	}
-
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-}
-
-func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Bad request", http.StatusBadRequest)
-		return
-	}
-	text := r.FormValue("text")
-	banner := r.FormValue("banner")
-
-	if strings.TrimSpace(text) == "" {
-		http.Error(w, "Input cannot be empty", http.StatusBadRequest)
-		return
-	}
-
-	if banner == "" {
-		http.Error(w, "Banner is required", http.StatusBadRequest)
-		return
-	}
-
-	fileName := "banners/" + banner + ".txt"
-
-	asciiMap, err := functions.BuildFontMap(fileName)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
-
-	result, err := functions.RenderAscii(text, asciiMap)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	template, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		http.Error(w, "Template not found", http.StatusNotFound)
-		return
-	}
-
-	data := PageData{
-		Result: result,
-		Text:   text,
-		Banner: banner,
-	}
-
-	err = template.Execute(w, data)
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-}
 func main() {
-
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	// registered a route to request a path to homeHandler
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/ascii-art", asciiArtHandler)
-
-	fmt.Print("Server Starting on :http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err)
-	}
+	server.TemplatesFS = templateFs
+	server.Style = Style
+	server.CssFS = cssFiles
+	server.NewServer()
 }
